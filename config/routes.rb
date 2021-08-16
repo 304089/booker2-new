84ddb943fffa3bc00b46f8d964fb1a242eaf9e3d
304089 <9457imai2@gmail.com>
2021-08-16 08:18:32 +0000

    [Finish]応用課題５

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   get '/home/about' => 'homes#about'
-
+  get '/search' => 'searches#search'
 
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
